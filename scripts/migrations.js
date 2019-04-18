15f63d686a8a05070c04d74e5ba5c24b685acc71
Isaias Vallejos <isaiasalansv@hotmail.com>
2019-04-18 03:58:02 -0300

    Update migrations to create design documents
    See #6

--- scripts/migrations.js
+++ scripts/migrations.js
@@ -7,16 +7,24 @@ import {
 } from 'vendor/couchdb/connection'
 
 import { argv } from 'yargs'
+import createDatabaseDesigns from 'database/design'
 
 const connection = createConnectionAsDefault()
 
-if (argv.up) {
-  createDatabase(process.env.COUCHDB_DATABASE, connection).then(() =>
+const down = () =>
+  destroyDatabase(process.env.COUCHDB_DATABASE, connection).then(() =>
     process.exit()
   )
+
+const up = () =>
+  createDatabase(process.env.COUCHDB_DATABASE, connection)
+    .then(createDatabaseDesigns)
+    .catch(down)
+
+if (argv.up) {
+  up()
 }
 
 if (argv.down) {
-  destroyDatabase(process.env.COUCHDB_DATABASE, connection)
-    .then(() => process.exit()) // prettier-ignore
+  down()
 }
