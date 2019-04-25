2d459baf04ac2379a5ee0a957a3278054578eb63
Isaias Vallejos <isaiasalansv@hotmail.com>
2019-04-25 03:28:36 -0300

    Remove winston format combine in log middleware

--- src/server/api/middlewares/log.js
+++ src/server/api/middlewares/log.js
@@ -5,10 +5,10 @@ expressWinston.requestWhitelist.push('body')
 
 export const loggerMiddleware = expressWinston.logger({
   transports: [new winston.transports.Console()],
-  format: winston.format.combine(winston.format.json())
+  format: winston.format.json()
 })
 
 export const errorLoggerMiddleware = expressWinston.errorLogger({
   transports: [new winston.transports.Console()],
-  format: winston.format.combine(winston.format.json())
+  format: winston.format.json()
 })
